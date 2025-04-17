d081820cff01e30b661a38b136f042db4d64564e
Manish Tomer <mt0578902@gmail.com>
2025-04-17 14:39:48 +0530

    Added MVC boilerplate for booking

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,47 @@
+const { StatusCodes } = require("http-status-codes");
+const AppError = require("../utils/errors/app-error");
+
+class CrudRepository {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async create(data) {
+        return await this.model.create(data);
+    }
+
+    async destroy(data) {
+        const response = await this.model.destroy({
+            where: { id: data }
+        })
+        if (!response) {
+            throw new AppError("Not able to find resource", StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+
+    async get(data) {
+        const response = await this.model.findByPk(data);
+        if (!response) {
+            throw new AppError("Not able to find resource", StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+
+
+    async getAll() {
+        return await this.model.findAll();
+    }
+
+    async update(id, data) {
+        const [response] = await this.model.update(data, {
+            where: { id: id }
+        })
+        if (!response) {
+            throw new AppError("Not able to find resource", StatusCodes.NOT_FOUND);
+        }
+        return response
+    }
+}
+
+module.exports = CrudRepository;
